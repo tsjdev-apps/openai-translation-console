edb0d7a48235e25036840ff49bf392e48ac38340
Sebastian Jensen <apps@tsjdev-apps.de>
2023-12-17 21:04:43 +0100

    Add model classes.

---
+++ OpenAITranslationConsole/OpenAITranslationConsole/Models/TranslationResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OpenAITranslationConsole.Models;
+
+/// <summary>
+///     Represents a response of the translation
+///     request.
+/// </summary>
+/// <param name="Text"> The translated text of the audio file. </param>
+internal record TranslationResponse(
+    [property: JsonPropertyName("text")] string? Text);
